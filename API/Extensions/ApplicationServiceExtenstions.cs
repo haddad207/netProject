ff657339205311fd09ebeed5c2d3e64171c2c237
Fadi <haddad0207@gmail.com>
2021-01-05 13:50:24 +0200

    end of Section 15

--- API/Extensions/ApplicationServiceExtenstions.cs
+++ API/Extensions/ApplicationServiceExtenstions.cs
@@ -18,6 +18,7 @@ namespace DatingApp.API.Extensions
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<ILikesRepository, LikesRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
             services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<LogUserActivity>();
--- API/Helpers/AutoMappersProfile.cs
+++ API/Helpers/AutoMappersProfile.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using API.DTOs;
 using API.Entities;
 using AutoMapper;
 using DatingApp.API.DTOs;
@@ -17,6 +18,13 @@ namespace DatingApp.API.Helpers
             CreateMap<Photo, PhotoDTO>();
             CreateMap<MemberUpdateDTO, AppUser>();
             CreateMap<RegisterDTO, AppUser>();
+            CreateMap<Message, MessageDTO>()
+                .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src =>
+                    src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(src =>
+                    src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
+                
+
         }
     }
 }
